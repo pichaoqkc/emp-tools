0c20158c0b679ca25e6de908febecec395a1955b
pichao <pichao@quarkchain.org>
2021-03-11 13:03:36 -0800

    trig redeploy

--- features/emp-selector/EMPs.ts
+++ features/emp-selector/EMPs.ts
@@ -5,6 +5,6 @@ export const EMPs: { [networkId: number]: string[] } = {
   ],
   42: [
     "0xc3b95bBE19f21fc51f7d3A47b8F7881d61e5D495", //cnyusd kovan mar 01
-    "0x0144D6CE156646348f5558B66204eC0309077e46", //eurusd kovan mar 01
+    // "0x0144D6CE156646348f5558B66204eC0309077e46", //eurusd kovan mar 01
   ],
 };
